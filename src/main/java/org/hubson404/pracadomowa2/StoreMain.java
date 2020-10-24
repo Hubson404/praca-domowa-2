75f3926785a19c6989d01996ba5a95bbdcf565dd
Hubson404 <60826441+Hubson404@users.noreply.github.com>
2020-10-24 13:13:11 +0200

    StorePlusService.run() randomization of product prices added.

--- src/main/java/org/hubson404/pracadomowa2/StoreMain.java
+++ src/main/java/org/hubson404/pracadomowa2/StoreMain.java
@@ -5,6 +5,8 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 @RequiredArgsConstructor
 public class StoreMain {
@@ -14,18 +16,13 @@ public class StoreMain {
     @EventListener(ApplicationReadyEvent.class)
     public void run() {
 
-        Product product1 = new Product("Trousers", 100);
-        Product product2 = new Product("Jacket", 300);
-        Product product3 = new Product("Shirt", 80);
-        Product product4 = new Product("Belt", 100);
-        Product product5 = new Product("Socks", 30);
-
-        storeService.addProductToShoppingCart(product1);
-        storeService.addProductToShoppingCart(product2);
-        storeService.addProductToShoppingCart(product3);
-        storeService.addProductToShoppingCart(product4);
-        storeService.addProductToShoppingCart(product5);
-
+        for (int i = 0; i < 5; i++) {
+            storeService.addProductToShoppingCart(
+                    new Product(
+                            "product " + i,
+                            (float) (50 + Math.random() * (300 - 50))
+                    ));
+        }
         storeService.calculateTotal();
 
     }
